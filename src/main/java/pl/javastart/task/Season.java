e753b02c51ad797edd31f6915583ed4fa134dfe0
kamilzyczynski <kamil.zyczynski@gmail.com>
2023-05-22 11:50:57 +0200

    Rozwiązanie

--- src/main/java/pl/javastart/task/Season.java
+++ src/main/java/pl/javastart/task/Season.java
@@ -1,5 +1,43 @@
 package pl.javastart.task;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public enum Season {
-    //uzupełnij
+    SPRING(new String[]{"marzec", "kwiecień", "maj"}, "wiosna"),
+    SUMMER(new String[]{"czerwiec", "lipiec", "sierpień"}, "lato"),
+    AUTUMN(new String[]{"wrzesień", "październik", "listopad"}, "jesień"),
+    WINTER(new String[]{"grudzień", "styczeń", "luty"}, "zima");
+
+    Season(String[] months, String plTranslation) {
+        this.months = months;
+        this.plTranslation = plTranslation;
+    }
+
+    private final String[] months;
+    private final String plTranslation;
+
+    public String[] getMonths() {
+        return months;
+    }
+
+    public String getPlTranslation() {
+        return plTranslation;
+    }
+
+    public static void findSeason(Scanner scanner) {
+        String choice = scanner.nextLine();
+        Season season = null;
+        for (Season value : Season.values()) {
+            if (choice.toLowerCase().equals(value.getPlTranslation())) {
+                season = value;
+            }
+        }
+        if (season != null) {
+            System.out.println("W tej porze roku są następujące miesiące:");
+            System.out.println(Arrays.toString(season.getMonths()));
+        } else {
+            System.out.println("Wprowadzono nieprawidłowe dane.");
+        }
+    }
 }
--- src/main/java/pl/javastart/task/SeasonManager.java
+++ src/main/java/pl/javastart/task/SeasonManager.java
@@ -6,7 +6,13 @@ public class SeasonManager {
 
     // uzupełnij metodę - do wczytywania danych użyj przekazany Scanner
     public void run(Scanner scanner) {
+        System.out.println("Podaj porę roku");
+        for (Season value : Season.values()) {
+            System.out.println(value.getPlTranslation());
+        }
 
+        Season.findSeason(scanner);
     }
+
 
 }
